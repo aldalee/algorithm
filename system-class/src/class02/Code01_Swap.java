81fa1c612f769d74621e95b3562ebfb2c69d5e7d
HuanyuLee <hadoop_2020@163.com>
2022-09-30 21:16:22 +0800

    第二节课 不用额外变量交换两个数

---
+++ system-class/src/class02/Code01_Swap.java
@@ -0,0 +1,26 @@
+package class02;
+
+import java.util.Arrays;
+
+/**
+ * 不用额外变量交换两个数
+ * @author HuanyuLee
+ * @date 2022/9/29
+ */
+public class Code01_Swap {
+    public static void swap(int[] arr, int i, int j) {
+        // 防止同一个内存区域，出现错误
+        if (i != j){
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println("交换前：arr = " + Arrays.toString(arr));
+        swap(arr, 0, 1);
+        System.out.println("交换前：arr = " + Arrays.toString(arr));
+    }
+}
