ace02f772949a3d2226db6ad68dabd6c6dcdc342
aldalee <hadoop_2020@163.com>
2023-04-28 21:20:01 +0800

    用双链表结构实现双端队列

---
+++ learn-system/src/ds/list/Code004_DoubleLinkedListToDeque.java
@@ -0,0 +1,96 @@
+package ds.list;
+
+/**
+ * 用双链表结构实现双端队列
+ * @author HuanyuLee
+ * @date 2023/4/28
+ */
+public class Code004_DoubleLinkedListToDeque<T> {
+    private DoubleListNode<T> head;
+    private DoubleListNode<T> tail;
+    private int size;
+
+    public Code004_DoubleLinkedListToDeque() {
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void pushHead(T value) {
+        DoubleListNode<T> cur = new DoubleListNode<>(value);
+        if (head == null) {
+            head = cur;
+            tail = cur;
+        } else {
+            cur.next = head;
+            head.prev = cur;
+            head = cur;
+        }
+        size++;
+    }
+
+    public void pushTail(T value) {
+        DoubleListNode<T> cur = new DoubleListNode<>(value);
+        if (head == null) {
+            head = cur;
+        } else {
+            tail.next = cur;
+            cur.prev = tail;
+        }
+        tail = cur;
+        size++;
+    }
+
+    public T pollHead() {
+        if (head == null) {
+            return null;
+        }
+        T ans = head.value;
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+        return ans;
+    }
+
+    public T pollTail() {
+        if (head == null) {
+            return null;
+        }
+        T ans = tail.value;
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+        return ans;
+    }
+
+    public T peekHead() {
+        T ans = null;
+        if (head != null) {
+            ans = head.value;
+        }
+        return ans;
+    }
+
+    public T peekTail() {
+        T ans = null;
+        if (tail != null) {
+            ans = tail.value;
+        }
+        return ans;
+    }
+}
---
+++ learn-system/test/ds/list/Code004DoubleLinkedListToDequeTest.java
@@ -0,0 +1,72 @@
+package ds.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class Code004DoubleLinkedListToDequeTest {
+    private static final int VALUE = 99999;
+    private static final int TEST_CASES = 100000;
+
+    private Code004_DoubleLinkedListToDeque<Integer> myDeque;
+    private Deque<Integer> deque;
+
+    @Before
+    public void init() {
+        myDeque = new Code004_DoubleLinkedListToDeque<>();
+        deque = new LinkedList<>();
+    }
+
+    @Test
+    public void testDeque() {
+        for (int i = 0; i < TEST_CASES; i++) {
+            if (myDeque.isEmpty() != deque.isEmpty()) {
+                fail();
+            }
+            if (myDeque.size() != deque.size()) {
+                fail();
+            }
+            double decide = Math.random();
+            switch ((int) (decide / 0.33)) {
+                case 0 -> {
+                    int num = (int) (Math.random() * VALUE);
+                    if (Math.random() < 0.5) {
+                        myDeque.pushHead(num);
+                        deque.addFirst(num);
+                    } else {
+                        myDeque.pushTail(num);
+                        deque.addLast(num);
+                    }
+                }
+                case 1 -> {
+                    if (!myDeque.isEmpty()) {
+                        if (Math.random() < 0.5) {
+                            assertEquals(myDeque.pollHead(), deque.pollFirst());
+                        } else {
+                            assertEquals(myDeque.pollTail(), deque.pollLast());
+                        }
+                    }
+                }
+                default -> {
+                    if (!myDeque.isEmpty()) {
+                        if (Math.random() < 0.5) {
+                            assertEquals(myDeque.peekHead(), deque.peekFirst());
+                        } else {
+                            assertEquals(myDeque.peekTail(), deque.peekLast());
+                        }
+                    }
+                }
+            }
+        }
+        assertEquals(myDeque.size(), deque.size());
+        while (!myDeque.isEmpty()) {
+            assertEquals(myDeque.pollHead(), deque.pollFirst());
+        }
+        assertTrue(myDeque.isEmpty());
+        assertTrue(deque.isEmpty());
+    }
+}
