60d5c603c726c30dc7e914d103064a9ab0ca7705
aldalee <hadoop_2020@163.com>
2023-05-09 22:59:02 +0800

    Randomly generate a double linked list based on the LENGTH and VALUE.

--- learn-system/test/tool/GenerateRandomDoubleList.java
+++ learn-system/test/tool/GenerateRandomDoubleList.java
@@ -4,20 +4,16 @@ import ds.list.DoubleListNode;
 
 import java.util.Random;
 
-/**
- * @author HuanyuLee
- * @date 2023/4/21
- */
 public class GenerateRandomDoubleList {
-    public static DoubleListNode generateRandomDoubleList(final int LENGTH, final int VALUE) {
+    public static DoubleListNode<Integer> generateRandomDoubleList(final int LENGTH, final int VALUE) {
         Random rand = new Random();
         int size = rand.nextInt(LENGTH + 1);
         if (size == 0) return null;
-        DoubleListNode head = new DoubleListNode(rand.nextInt(VALUE + 1));
-        DoubleListNode pre = head;
-        DoubleListNode cur;
+        DoubleListNode<Integer> head = new DoubleListNode<Integer>(rand.nextInt(VALUE + 1));
+        DoubleListNode<Integer> pre = head;
+        DoubleListNode<Integer> cur;
         while (--size != 0) {
-            cur = new DoubleListNode(rand.nextInt(VALUE + 1));
+            cur = new DoubleListNode<Integer>(rand.nextInt(VALUE + 1));
             pre.next = cur;
             cur.prev = pre;
             pre = cur;
