eb8dc3ee0c5bc20f6e59a1537d0e662ccd28ec33
aldalee <hadoop_2020@163.com>
2023-05-15 20:52:37 +0800

    Definition for singly-linked list.

---
+++ learn-system/src/ds/list/ListNode.java
@@ -0,0 +1,23 @@
+package ds.list;
+
+/**
+ * Definition for singly-linked list.
+ * @author HuanyuLee
+ * @date 2023/4/20
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
