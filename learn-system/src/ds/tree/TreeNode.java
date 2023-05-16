17f8b67f818f66077b069b4a68e67fa51cf83ffc
aldalee <hadoop_2020@163.com>
2023-05-16 14:04:30 +0800

    Definition for a binary tree node.

---
+++ learn-system/src/ds/tree/TreeNode.java
@@ -0,0 +1,25 @@
+package ds.tree;
+
+/**
+ * Definition for a binary tree node.
+ * @author HuanyuLee
+ * @date 2023/4/16
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
