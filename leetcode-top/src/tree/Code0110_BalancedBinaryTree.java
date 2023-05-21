d816d84928bdbcd9c830346ef592f60ace6b7b4c
aldalee <hadoop_2020@163.com>
2023-05-21 21:32:37 +0800

    平衡二叉树

--- leetcode-top/src/tree/Code0110_BalancedBinaryTree.java
+++ leetcode-top/src/tree/Code0110_BalancedBinaryTree.java
@@ -8,33 +8,10 @@ package tree;
  */
 public class Code0110_BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
-        return f(root).balance;
-    }
-    //方法一: 课上代码
-    static class Info {
-        boolean balance;
-        int height;
-
-        public Info(boolean balance, int height) {
-            this.balance = balance;
-            this.height = height;
-        }
+        return height(root) >= 0;
     }
 
-    Info f(TreeNode root) {
-        if (root == null) {
-            return new Info(true, 0);
-        }
-        Info L = f(root.left);
-        Info R = f(root.right);
-        int height = Math.max(L.height, R.height) + 1;
-        boolean isBalanced = L.balance && R.balance && Math.abs(L.height - R.height) <= 1;
-        return new Info(isBalanced, height);
-    }
-
-    //方法2: LeetCode 效率更高
-    //调用时: return height(root) >= 0;
-    int height(TreeNode root) {
+    private int height(TreeNode root) {
         if (root == null) {
             return 0;
         }
