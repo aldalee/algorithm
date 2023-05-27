590ad96c56d54ff90671d9b503793080468d7060
aldalee <hadoop_2020@163.com>
2023-05-27 18:16:23 +0800

    JZ51 数组中的逆序对

---
+++ learn-system/src/algorithm/sorting/ReversePairsInArrays.java
@@ -0,0 +1,50 @@
+package algorithm.sorting;
+
+/**
+ * （牛客网）JZ51 数组中的逆序对
+ * @author HuanyuLee
+ * @date 2023/5/27
+ */
+public class ReversePairsInArrays {
+    public int InversePairs(int[] array) {
+        if (array == null || array.length < 2) {
+            return 0;
+        }
+        int ans = process(array, 0, array.length - 1);
+        return ans % 1000000007;
+    }
+
+    // arr[l..r]既要排好序，又要返回逆序对数量
+    private int process(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int mid = l + ((r - l) >> 1);
+        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+    }
+
+    private int merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = help.length - 1;
+        int p1 = m, p2 = r;     // 从右向左遍历
+        int ans = 0;
+        while (p1 >= l && p2 > m) {
+            boolean comp = arr[p1] > arr[p2];
+            if (comp) {
+                ans += p2 - m;
+                ans %= 1000000007;
+            }
+            help[i--] = comp ? arr[p1--] : arr[p2--];
+        }
+        while (p1 >= l) {
+            help[i--] = arr[p1--];
+        }
+        while (p2 > m) {
+            help[i--] = arr[p2--];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+        return ans;
+    }
+}
