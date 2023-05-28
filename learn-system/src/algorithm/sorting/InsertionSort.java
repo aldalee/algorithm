8fa2c799c8f2a99de485ecf97306cee7dd7bfd13
aldalee <hadoop_2020@163.com>
2023-05-28 13:49:40 +0800

    插入排序

---
+++ learn-system/src/algorithm/sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package algorithm.sorting;
+
+/**
+ * 插入排序
+ * @author HuanyuLee
+ * @date 2023/5/10
+ */
+public class InsertionSort {
+    public void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {  // [0...i]做到有序
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
