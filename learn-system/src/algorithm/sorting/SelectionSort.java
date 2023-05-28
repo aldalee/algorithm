a0703b581e2e3181a7a0ff426d5b28ca7c08693a
aldalee <hadoop_2020@163.com>
2023-05-28 13:39:01 +0800

    选择排序

---
+++ learn-system/src/algorithm/sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package algorithm.sorting;
+
+/**
+ * 选择排序
+ * @author HuanyuLee
+ * @date 2023/5/10
+ */
+public class SelectionSort {
+    public void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            // [i...n-1]上找最小值的下标
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        if (i == j) return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
