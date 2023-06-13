4037628a731933be0d2e5693049fdb221b80130e
aldalee <hadoop_2020@163.com>
2023-06-13 23:43:07 +0800

    滑动窗口最大值

---
+++ leetcode-top/src/slidingwindow/Code0239_SlidingWindowMaximum.java
@@ -0,0 +1,37 @@
+package slidingwindow;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode.cn/problems/sliding-window-maximum
+ * 滑动窗口最大值
+ * @author HuanyuLee
+ * @date 2023/6/9
+ */
+public class Code0239_SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < k; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+
+        int[] ans = new int[n - k + 1];
+        ans[0] = nums[deque.peekFirst()];
+        for (int i = k; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return ans;
+    }
+}
